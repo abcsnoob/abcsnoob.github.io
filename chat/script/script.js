ba53ea8c5d011119ed54c017e0dde0649ebbdc8c
Abc's Noob <abcsspprt@gmail.com>
2026-01-31 18:10:08 +0700

    changespinneronsharebutton

--- chat/script/script.js
+++ chat/script/script.js
@@ -310,6 +310,48 @@ textarea {
 ::-webkit-scrollbar-track { background: transparent; }
 ::-webkit-scrollbar-thumb { background: var(--border); border-radius: 10px; }
 ::-webkit-scrollbar-thumb:hover { background: var(--text-muted); }
+
+.lds-ring,
+.lds-ring div {
+  box-sizing: border-box;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid currentColor;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: currentColor transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+
             `;
             document.head.appendChild(styleTag);
         }
@@ -704,7 +746,7 @@ async exportSecure() {
         URL.revokeObjectURL(downloadUrl); // Giải phóng bộ nhớ
         // --- KẾT THÚC: TẢI FILE ---
 
-        shareBtn.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> Uploading...';
+        shareBtn.innerHTML = ' <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Uploading...';
         shareBtn.disabled = true;
 
         const formData = new FormData();
@@ -1095,9 +1137,3 @@ async deleteSession(id) {
 
 // Khởi chạy khi Window Load
 window.addEventListener('DOMContentLoaded', () => NoobEngine.init());
-
-
-
-
-
-
